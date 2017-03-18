9968b431dbc914627c508752cca67177416df171
Faizaan Charania <faizaancharania@gmail.com>
2017-03-18 18:24:33 +0530

    worked on neural network, using LSTM

--- attempted_nn.py
+++ attempted_nn.py
@@ -1,21 +1,39 @@
-# from sklearn.neural_network import MLPClassifier
-# from sklearn.metrics import accuracy_score as accuracy
-# clf = MLPClassifier(solver='adam', alpha=1e-5, hidden_layer_sizes=(64, 32), random_state=1,verbose=True)
-# import pandas as pd
-# import scipy.io as spio
-#
-# left_signals = spio.loadmat('./left_signals')['left_signals']
-#
-#
+from sklearn.neural_network import MLPClassifier
+from sklearn.metrics import accuracy_score as accuracy
 import numpy as np
-from keras.models import Sequential
-from keras.layers import Dense, Activation,Dropout,LSTM
-# import pandas as pd
 from numpy import *
 import random
-from keras.models import load_model
+import pandas as pd
+from sklearn.preprocessing import scale
 
-# # import pdb; pdb.set_trace()
+clf = MLPClassifier(solver='adam',activation='relu', alpha=0.005, hidden_layer_sizes=(10,5),verbose=True
+,warm_start=True,tol=0.000001, learning_rate='invscaling')
+
+
+# import pdb; pdb.set_trace()
+#################################################
+# solver    activation      score (most of the times)
+#
+# lbfgs     tanh            0.5
+# lbfgs     identity        0.5
+# lbfgs     relu            0.5
+# lbfgs     logistic        0.5
+#
+# sgd       relu            0.5
+# sgd       tanh            0.5
+# sgd       identity        0.5
+# sgd       logistic        0.5
+#
+# adam      relu            0.5
+# adam      tanh            0.5
+# adam      identity        0.5
+# adam      logistic        0.5
+#
+#
+# from keras.models import Sequential
+# from keras.layers import Dense, Activation,Dropout,LSTM
+# from keras.models import load_model
+
 # model = Sequential()
 # model.add(LSTM(64,input_shape=(1375,132),return_sequences=True))
 # model.add(LSTM(32, return_sequences=True, inner_activation='sigmoid', activation='hard_sigmoid'))
@@ -29,51 +47,56 @@ from keras.models import load_model
 #
 #
 # model.save('my_model.h5')
-
-model = load_model('my_model.h5')
+#
+# model = load_model('my_model.h5')
 
 left = pd.read_csv('W_into_left.csv',header=None)
 right = pd.read_csv('W_into_right.csv',header=None)
+print "done"
 left = left.transpose()
 right = right.transpose()
 labels = []
-for i in range(len(left)-(1375*5)):
+for i in range(len(left)-1375*5):
     labels.append(0)
 
-for i in range(len(right)-(1375*5)):
+for i in range(len(right)-1375*5):
     labels.append(1)
 
-data = left.as_matrix()[:-(1375*5)][:]
-data = vstack((data,right.as_matrix()[:-(1375*5)][:]))
+data = left.as_matrix()[:-(1375*5),:]
+data = vstack((data,right.as_matrix()[:-(1375*5),:]))
 
+# data = c_[left.as_matrix()[:-5,-10:] + left.as_matrix()[:-5,:10]]
+# data = vstack((data,c_[right.as_matrix()[:-(5),-10:] + right.as_matrix()[:-5,:10]]))
 
+all_data = []
 all_data = c_[data,labels]
 random.shuffle(all_data)
 labels = all_data[:,-1:]
 
 data = all_data[:,:-1]
-data = reshape(data,(119,1375,132))
+data = scale(data)
+# data = reshape(data,(119,1375,132))
+
+for i in range(200):
+    print "**",i
+    clf.fit(data,labels.ravel())
 
 
-labels = list(labels)
-# clf.fit(data,labels)
-# ypred = clf.predict(test_data)
-# score = accuracy(test_labels,ypred)
-
-# data = np.random.random((1000, 784))
-# labels = np.random.randint(2, size=(1000, 1))
-# test_data = np.random.random((200, 784))
-# test_labels = np.random.randint(2, size=(200, 1))
-# data = np.reshape(data,data.shape + (1,))
-
-
-model.fit(data, labels, nb_epoch=10,batch_size=1)
+# model.fit(data, labels, nb_epoch=10,batch_size=1)
 
 test_labels = [0 for i in range(1375*5)]
 test_labels += [1 for i in range(1375*5)]
-test_data = left.as_matrix()[-(1375*5):][:]
-test_data = vstack((test_data, right.as_matrix()[-(1375*5):][:]))
+test_data = left.as_matrix()[-(1375*5):,:]
+test_data = vstack((test_data, right.as_matrix()[-(1375*5):,:]))
+test_data = scale(test_data)
+# test_labels = [0 for i in range(5)]
+# test_labels += [1 for i in range(5)]
+# test_data = c_[left.as_matrix()[-5:,-10:] + left.as_matrix()[-5:,:10]]
+# test_data = vstack((test_data,c_[right.as_matrix()[-(5):,-10:] + right.as_matrix()[-5:,:10]]))
 
 
-score,acc = model.evaluate(test_data, test_labels, batch_size=16,show_accuracy=True)
-print score + "\n" + acc + "\n"
+ypred = clf.predict(test_data)
+score = accuracy(test_labels,ypred)
+print score
+# score,acc = model.evaluate(test_data, test_labels, batch_size=16,show_accuracy=True)
+# print score + "\n" + acc + "\n"
