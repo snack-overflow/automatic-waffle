15d7e81e1d6e85e286c28d21c76913c3707338f6
Faizaan Charania <faizaancharania@gmail.com>
2017-04-02 14:27:14 +0530

    added function in channel_selection.py to create scaled training and testing data using csp channels

--- channel_selection.py
+++ channel_selection.py
@@ -1,4 +1,7 @@
 import pandas as pd
+from sklearn.preprocessing import scale
+from numpy import *
+
 
 def get_channel_ranks(file_name):
 	matrix = pd.read_csv(file_name,header=None)
@@ -14,4 +17,26 @@ def get_channel_ranks(file_name):
 	df = pd.DataFrame(rank)
 	df.to_csv('ranks-'+file_name)
 
-get_channel_ranks('ranks.csv')
+# get_channel_ranks('ranks.csv')
+
+
+def use_csp_channels(subject="",channels=""):
+	left = pd.read_csv('./signals/'+subject+'-left-signals.csv',header=None)
+	right = pd.read_csv('./signals/'+subject+'-right-signals.csv',header=None)
+	print("data loaded")
+	data = left.as_matrix()[:-(1375*10)]
+	data = vstack((data,right.as_matrix()[:-(10*1375)][:]))
+	test_data = left.as_matrix()[-(1375*10):]
+	test_data = vstack((test_data,right.as_matrix()[-(10*1375):][:]))
+	channels = pd.read_csv(channels,header=None)
+	channels = list(channels)
+	data= data[:,(channels)]
+	data = scale(data)
+	test_data = test_data[:,(channels)]
+	test_data = scale(test_data)
+	data = pd.DataFrame(data)
+	test_data = pd.DataFrame(test_data)
+	data.to_csv('scaled-training-data-'+subject+'.csv')
+	test_data.to_csv('scaled-testing-data-'+subject+'.csv')
+
+use_csp_channels('A02T','selected_channels.csv')
